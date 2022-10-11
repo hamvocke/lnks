830fdfab1be92e5c9cb9e882d5013dc889338cdb
Ham Vocke <hermann.vocke@gmail.com>
2022-10-11 22:22:16 +0200

    suppress warnings when opening URL

--- bookmarks.sh
+++ bookmarks.sh
@@ -4,4 +4,4 @@ if ! [ -x "$(command -v fzf)" ]; then
     echo "fzf is not installed"
 fi
 
-cat bookmarks.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks >" --with-nth='1..-2' --bind='enter:execute(xdg-open {-1})+abort'
+cat bookmarks.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks >" --with-nth='1..-2' --bind='enter:execute(xdg-open {-1} 2>/dev/null)'
