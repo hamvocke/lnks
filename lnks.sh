3a7db765a5fd8a96c3e4269201629871ae7462dc
Ham Vocke <hvocke@stackoverflow.com>
2022-10-25 10:36:36 +0200

    determine open command based on OS

--- lnks.sh
+++ lnks.sh
@@ -5,4 +5,13 @@ if ! [ -x "$(command -v fzf)" ]; then
     exit 1
 fi
 
-cat *.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks >" --with-nth='1..-2' --bind='enter:execute(xdg-open {-1} 2>/dev/null)' --preview='echo {-1}' --preview-window='up,1'
+
+case "$OSTYPE" in
+  darwin*)  OPEN_COMMAND="open" ;;
+  linux*)   OPEN_COMMAND="xdg-open" ;;
+  *)        echo "unsupported OD: $OSTYPE" && exit 1 ;;
+esac
+
+ENTER_COMMAND="enter:execute(${OPEN_COMMAND} {-1} 2>/dev/null)"
+
+cat *.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
