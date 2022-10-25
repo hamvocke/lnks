a1f03b0c7043a594e0d0fb7524934ba9d8b46dba
Ham Vocke <hvocke@stackoverflow.com>
2022-10-25 10:35:53 +0200

    exit if fzf is not installed

--- lnks.sh
+++ lnks.sh
@@ -2,6 +2,7 @@
 
 if ! [ -x "$(command -v fzf)" ]; then
     echo "fzf is not installed"
+    exit 1
 fi
 
 cat *.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks >" --with-nth='1..-2' --bind='enter:execute(xdg-open {-1} 2>/dev/null)' --preview='echo {-1}' --preview-window='up,1'
