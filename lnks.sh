c0700b32e2a8801821bb51e01b69ed852f693b63
Gregor Casar <gregorcasar@gmail.com>
2022-11-13 19:52:50 +0100

    only open single selection in bash

--- lnks.sh
+++ lnks.sh
@@ -12,6 +12,13 @@ case "$OSTYPE" in
   *)        echo "unsupported OD: $OSTYPE" && exit 1 ;;
 esac
 
-ENTER_COMMAND="enter:execute(${OPEN_COMMAND} {-1} 2>/dev/null)"
+SELECTION=$(cat "$(dirname "$0")"/*.txt | fzf \
+  --border=rounded --margin=5% \
+  --prompt="Search Bookmarks > " \
+  --with-nth='1..-2' \
+  --preview='echo {-1}' --preview-window='up,1')
 
-cat "$(dirname "$0")"/*.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
+if [ -n "$SELECTION" ]; then
+    TARGET_LINK=$(echo $SELECTION | sed 's/^.*\(https.*\)$/\1/')
+    $OPEN_COMMAND $TARGET_LINK 2>/dev/null
+fi
