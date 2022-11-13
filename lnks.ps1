33abb2262ca861fac6aa32ce707a5d784e4e1567
Gregor Casar <gregorcasar@gmail.com>
2022-11-13 17:47:30 +0100

    make lnks.ps1 work on windows

--- lnks.ps1
+++ lnks.ps1
@@ -1,12 +1,10 @@
 #!/usr/bin/env pwsh
 
-$targetLink = cat ./bookmarks.txt | `
-  fzf `
-    --border=rounded --margin=5% `
-    --prompt="Search Bookmarks > " `
-    --with-nth='1..-2' `
-    --preview='echo {-1}' --preview-window='up,1' | `
-  sed 's/^.*\(https.*\)$/\1/'
+$selection = Get-Content ./bookmarks.txt | fzf `
+  --border=rounded --margin=5% `
+  --prompt="Search Bookmarks > " `
+  --with-nth='1..-2' `
+  --preview='echo {-1}' --preview-window='up,1'
 
+$targetLink = $selection.Split(' ')[-1]
 Start-Process $targetLink
-
