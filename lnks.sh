5df5283b13197e0821d91349bf3710b06ad31698
Ed Morrell <morrell53@Eds-MacBook-Pro.local>
2022-11-14 14:57:31 +0000

    Modified search path

--- lnks.sh
+++ lnks.sh
@@ -14,4 +14,4 @@ esac
 
 ENTER_COMMAND="enter:execute(${OPEN_COMMAND} {-1} 2>/dev/null)"
 
-cat *.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
+cat ~/lnks/*.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
