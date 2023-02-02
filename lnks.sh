480dc2d07d78f5fc72a2a962e2248db3847ca9fa
Ed Morrell <morrell53@Eds-MacBook-Pro.local>
2023-02-02 15:08:07 +0000

    Added functionality to automatically update Bookmarks.txt with Chrome bookmarks

--- lnks.sh
+++ lnks.sh
@@ -12,6 +12,18 @@ case "$OSTYPE" in
   *)        echo "unsupported OD: $OSTYPE" && exit 1 ;;
 esac
 
+DEFAULT_BROWSER=$(sh $(dirname "$0")/get_default_browser.sh)
+
+# If chrome being used as default browser then updates the bookmarks file with Chrome bookmarks
+if [[ $DEFAULT_BROWSER = "com.google.chrome" || $DEFAULT_BROWSER = "google-chrome.desktop" ]]; then
+  if ! [ -x "$(command -v jq)" ]; then
+    echo "jq is not installed"
+    exit 1
+  else
+    echo "$(sh $(dirname "$0")/get_chrome_bookmarks.sh)" > ~/lnks/bookmarks.txt
+  fi;
+fi;
+
 ENTER_COMMAND="enter:execute(${OPEN_COMMAND} {-1} 2>/dev/null)"
 
-cat ~/lnks/*.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
+cat "$(dirname "$0")"/*.txt | fzf --border=rounded --margin=5% --prompt="Search Bookmarks > " --with-nth='1..-2' --bind="${ENTER_COMMAND}" --preview='echo {-1}' --preview-window='up,1'
