bb42affeea65b4ef6f84e68f0ed7b49d107994c4
Ham Vocke <ham@hamvocke.com>
2023-03-28 20:12:41 +0200

    add --keep-open parameter

--- lnks.sh
+++ lnks.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+set -o errexit
+set -o nounset
+
 if ! [ -x "$(command -v fzf)" ]; then
     echo "fzf is not installed"
     exit 1
 fi
 
+keep_open=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -k|--keep-open) keep_open=true ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
 case "$OSTYPE" in
-  darwin*)  OPEN_COMMAND="open" ;;
-  linux*)   OPEN_COMMAND="xdg-open" ;;
+  darwin*)  open_command="open" ;;
+  linux*)   open_command="xdg-open" ;;
   *)        echo "unsupported OS: $OSTYPE" && exit 1 ;;
 esac
 
-ENTER_COMMAND="enter:execute-silent(${OPEN_COMMAND} {-1})"
+enter_command="enter:execute-silent(${open_command} {-1})"
+
+
+if [ "$keep_open" = false ]; then
+    enter_command="${enter_command}+abort"
+fi
 
 cat "$(dirname "$0")"/*.txt | fzf \
   --border=rounded \
   --margin=5% \
   --prompt="Search Bookmarks > " \
   --with-nth='1..-2' \
-  --bind="${ENTER_COMMAND}" \
+  --bind="${enter_command}" \
   --preview='echo {-1}' \
   --preview-window='up,1'
